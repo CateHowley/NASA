d550c178716d7dbf2f5400b2430d43ba46e8f39e
catehowley <Catherine_howley25@milton.edu>
2024-04-23 19:53:17 -0400

    finshed product

--- src/ReadJson.java
+++ src/ReadJson.java
@@ -1,9 +1,12 @@
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -28,6 +31,12 @@ public class ReadJson {
 
         public JFrame mainframe;
         private JLabel statusLabel;
+        private JPanel imagePanel;
+        private JLabel imageLabel;
+        private String image;
+
+        private String title;
+        private String description;
 
         private JPanel controlPanel;
         private JMenuBar mb;
@@ -36,9 +45,10 @@ public class ReadJson {
         private JTextArea ta; //typing area
         private int WIDTH = 900;
         private int HEIGHT = 700;
+    private JLabel statusLabel2;
 
 
-        public static void main(String args[]) throws ParseException {
+        public static void main(String args[]) throws ParseException, IOException {
             //prepareGUI();
 
 
@@ -64,7 +74,7 @@ public class ReadJson {
 
 
 
-        public ReadJson() throws ParseException {
+        public ReadJson() throws ParseException, IOException {
             pull();
             prepareGUI();
 
@@ -72,7 +82,7 @@ public class ReadJson {
         }
 
 
-        private void prepareGUI() {
+        private void prepareGUI() throws IOException {
             mainframe = new JFrame("Java SWING Examples");
             mainframe.setSize(WIDTH, HEIGHT);
             mainframe.setLayout(new GridLayout(3, 1));
@@ -89,6 +99,16 @@ public class ReadJson {
 
             statusLabel = new JLabel("Label", JLabel.CENTER);
             statusLabel.setSize(250, 100);
+            statusLabel2 = new JLabel("Label", JLabel.CENTER);
+            statusLabel2.setSize(250, 100);
+
+            mainframe.add(statusLabel);
+            imagePanel=new JPanel();
+            addImage();
+            mainframe.add(statusLabel2);
+            statusLabel.setText(title);
+            statusLabel2.setText(description);
+
 
             mainframe.addWindowListener(new WindowAdapter() {
                 public void windowClosing(WindowEvent windowEvent) {
@@ -145,7 +165,93 @@ public class ReadJson {
 
 
     }
+    private void addImage() throws IOException {
+        try {
+            String path = "";
+//                if (!ta.getText().contains("http")) {
+            path = "https://i.pinimg.com/originals/07/16/ba/0716ba54fe3b77b3a5b0b16c7bc33389.png";
+//                } else {
+//                    path = ta.getText();
+//                    if (path.contains("url")) {
+//                        path = path.substring(path.indexOf("http"));
+//                    }
+//                }
 
+            URL url = new URL(image);
+            BufferedImage ErrorImage = ImageIO.read(new File("images.png"));
+            BufferedImage inputImageBuff = ImageIO.read(url.openStream());
+            // weiYing =
+
+            ImageIcon inputImage;
+            if (inputImageBuff != null) {
+                inputImage = new ImageIcon(inputImageBuff.getScaledInstance(700, 300, Image.SCALE_SMOOTH));
+                // = new JLabel();
+                if (inputImage != null) {
+                    imageLabel = new JLabel(inputImage);
+                } else {
+                    imageLabel =new JLabel(new ImageIcon(ErrorImage.getScaledInstance(800, 589, Image.SCALE_SMOOTH)));
+
+                }
+                imagePanel.removeAll();
+                imagePanel.add(imageLabel);
+                 mainframe.add(imagePanel, BorderLayout.CENTER);
+
+            }
+
+        } catch(Exception e ){
+            System.out.println(e);
+            System.out.println("error");
+        }
+        mainframe.setVisible(true);
+
+
+        // private void showEventDemo() {
+
+//            JButton okButton = new JButton("button 1");
+//            JButton submitButton = new JButton("button 2");
+//            JButton cancelButton = new JButton("button 3");
+//            JButton myButton = new JButton("button 4");
+//            JButton helloButton = new JButton("button 5");
+//            JButton sixButton = new JButton("button 6");
+//            JButton sevenButton = new JButton("button 7");
+//            JButton eightButton = new JButton("button 8");
+//            JButton nineButton = new JButton("button 9");
+//            JButton tenButton = new JButton("button 10");
+
+//
+//            okButton.setActionCommand("button 1");
+//            submitButton.setActionCommand("button 2");
+//            cancelButton.setActionCommand("button 3");
+//            myButton.setActionCommand("button 4");
+//            myButton.setActionCommand("button 5");
+//            sixButton.setActionCommand("button 6");
+//            sevenButton.setActionCommand("button 7");
+//            eightButton.setActionCommand("button 8");
+//            nineButton.setActionCommand("button 9");
+//            tenButton.setActionCommand("button 10");
+//
+//            okButton.addActionListener(new ButtonClickListener());
+//            submitButton.addActionListener(new ButtonClickListener());
+//            cancelButton.addActionListener(new ButtonClickListener());
+//            myButton.addActionListener(new ButtonClickListener());
+//            helloButton.addActionListener(new ButtonClickListener());
+//
+//
+//            mainFrame.add(okButton);
+//            mainFrame.add(submitButton);
+//            mainFrame.add(cancelButton);
+//            mainFrame.add(myButton);
+//            mainFrame.add(helloButton);
+//            mainFrame.setVisible(true);
+//            mainFrame.add(sixButton);
+//            mainFrame.add(sevenButton);
+//            mainFrame.add(eightButton);
+//            mainFrame.add(nineButton);
+//            mainFrame.add(tenButton);
+//            controlPanel.add(nineButton,BorderLayout.EAST);
+//            controlPanel.add(tenButton,BorderLayout.WEST);
+//            mainFrame.setVisible(true);
+    }
 
 public void pull() throws ParseException {
             String output = "abc";
@@ -193,6 +299,9 @@ public void pull() throws ParseException {
                 System.out.println(jsonObject.get("title"));
                 System.out.println(jsonObject.get("explanation"));
                 System.out.println(jsonObject.get("url"));
+                image = (String) jsonObject.get("url");
+                title= (String)jsonObject.get("title");
+                description = (String) jsonObject.get("explanation");
 
 //            org.json.simple.JSONObject msg = (org.json.simple.JSONObject) jsonObject.get(0);
 //            System.out.println(msg.get("image"));
